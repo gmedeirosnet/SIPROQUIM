7d616ca6f6093d729fda171474fe547e0a8e086e
Gutemberg Medeiros <gmedeiros@gmail.com>
2025-04-15 08:24:39 +0100

    Update default EC2 instance type to t2.micro in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.small"
+  default     = "t2.micro"
 }
 
 variable "volume_size" {
