da83eba303a27fba40a8fd2e7dc258f96e22deb2
Gutemberg Medeiros <gmedeiros@gmail.com>
2025-08-13 16:11:39 +0100

    Update outputs to use variables for Elastic IP and public IP address

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "instance_id" {
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_eip.siproquim_eip.public_ip
+  value       = var.ec2_eip
 }
 
 output "instance_public_dns" {
@@ -15,12 +15,12 @@ output "instance_public_dns" {
 
 output "elastic_ip" {
   description = "Elastic IP associated with the instance"
-  value       = aws_eip.siproquim_eip.public_ip
+  value       = var.ec2_eip
 }
 
 output "elastic_ip_allocation_id" {
   description = "Allocation ID of the Elastic IP"
-  value       = aws_eip.siproquim_eip.id
+  value       = var.ec2_eip_allocation
 }
 
 output "security_group_id" {
@@ -30,5 +30,5 @@ output "security_group_id" {
 
 output "web_url" {
   description = "URL to access the web application"
-  value       = "http://${aws_eip.siproquim_eip.public_ip}"
+  value       = "http://${var.ec2_eip}"
 }
