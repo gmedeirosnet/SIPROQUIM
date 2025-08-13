1fc913213fcac9803b0fec30abf07a387e4cd4c5
Gutemberg Medeiros <gmedeiros@gmail.com>
2025-08-13 15:57:54 +0100

    Refactor resource tagging to use common_tags variable for consistency across resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,3 +39,15 @@ variable "environment" {
   type        = string
   default     = "production"
 }
+
+variable "common_tags" {
+  description = "Common tags to be applied to all resources"
+  type        = map(string)
+  default     = {
+    project = "SIPROQUIM"
+    env     = "production"
+    owner   = "Gilmar Trindade"
+    po      = "Gutemberg Medeiros"
+  }
+}
+
